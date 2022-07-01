edceba86d88180ca0df8cf13b202b11accbb165e
arana2743 <abhisekrana777@gmail.com>
2022-07-01 17:24:27 +0530

    update: app.py - model setup for db interaction with orm

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 from flask import Flask, redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+import string
+from datetime import datetime
+from random import choices
+
+
 app = Flask(__name__)
 
 # database setting - SQLite db used
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 # setting up db object, ORM - SQLAlchemy 
 db = SQLAlchemy(app)
+
+
+# defining the models
+class Link(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  original_url = db.Column(db.String(512))
+  short_url = db.Column(db.String(5), unique=True)
+  visits = db.Column(db.Integer, default=0)
+  date_created = db.Column(db.DateTime,  default=datetime.now)
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self.short_url = self.generate_short_link()
+
+  def generate_short_link(self):
+    characters = string.digits + string.ascii_letters
+    short_url = ''.join(choices(characters, k=5))
+    
+    link = self.query.filter_by(short_url=short_url).first()
+    if link:
+      return self.generate_short_link()
+    
+    return short_url
 
 
 # API routes
