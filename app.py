dd93cf8d5d41d9d84db1b6b401d1aeda08e9ef67
arana2743 <abhisekrana777@gmail.com>
2022-07-01 17:18:34 +0530

    update: app.py - database SQLite setup and added all other relevant endpoints skeleton

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, redirect, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# database setting - SQLite db used
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# setting up db object, ORM - SQLAlchemy 
+db = SQLAlchemy(app)
+
 
 # API routes
 # main root url - just used here to check if API is working
@@ -11,6 +20,24 @@ def hello_world():
     "desc": "A url shortening api service."
   }
   return jsonify(data)
+
+# /shorten_url = used to pass long_url to shorten it as json 
+# format : { 'original_url': 'https://www.example.com/ }
+@app.route('/shorten_url', methods=['POST'])
+def shorten_url():
+  pass
+
+
+# /<short_url> = used to redirect from short_url to original_url 
+@app.route('/<short_url>')
+def redirect_to_url(short_url):
+  pass
+
+
+@app.route('/stats', methods=['GET'])
+def get_all_stats():
+  pass
+
 
 
 # main call for the app
